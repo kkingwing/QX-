625fb80c05f5e8869536f3b1c4e5261e8dec0f72
Qing Yi <495017921@qq.com>
2024-08-21 13:52:35 +0800

    fix

--- dml.js
+++ dml.js
@@ -17,9 +17,9 @@ Surge、QuantumultX、Loon、Shadowrocket:
 3.关闭获取ck脚本，避免产生不必要的mitm。
 
 [Script]
-cron "30 10 * * *" script-path=https://gist.githubusercontent.com/Sliverkiss/6b4da0d367d13790a9fd1d928c82bdf8/raw/dlm.js,timeout=300, tag=达美乐披萨
+cron "30 10 * * *" script-path=https://raw.githubusercontent.com/kkingwing/QX-/main/dml.js,timeout=300, tag=达美乐披萨
 
-http-request ^https:\/\/game\.dominos\.com\.cn\/.+\/game\/gameDone script-path=https://gist.githubusercontent.com/Sliverkiss/6b4da0d367d13790a9fd1d928c82bdf8/raw/dlm.js,requires-body=true, timeout=10, tag=达美乐披萨获取token
+http-request ^https:\/\/game\.dominos\.com\.cn\/.+\/game\/gameDone script-path=https://raw.githubusercontent.com/kkingwing/QX-/main/dml.js,requires-body=true, timeout=10, tag=达美乐披萨获取token
 [MITM]
 hostname =game.dominos.com.cn
 
