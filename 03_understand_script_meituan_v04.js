e8cb9800d310be4bc218bfe1015c773413455ef1
Qing Yi <495017921@qq.com>
2024-09-10 20:38:35 +0800

    fix

--- 03_understand_script_meituan_v04.js
+++ 03_understand_script_meituan_v04.js
@@ -26,27 +26,31 @@ function sign() {
     const url = {url: signurlVal, headers: JSON.parse(signheaderVal), body: signBodyVal}
     qxApi.post(url, (error, response, data) => {
         qxApi.log(`${cookieName}, data: ${data}`)
-        const result = JSON.parse(data)
-        let subTitle = ``
-        let detail = ``
+        if (data) {
+            const result = JSON.parse(data)
+            let subTitle = ``
+            let detail = ``
 
-        if (result.code == 0) {
-            subTitle = '签到结果: 成功'
-            detail = `本周连签: ${result.data[0].circleSignTimes}天`
-            for (r of result.data[0].signInAwardRecords) {
-                const rinfo = JSON.parse(r.info)
-                if (rinfo.type == `cash`) detail += `, 奖励金: ${rinfo.amount / 100}元`
-                else if (rinfo.type == `coupon`) detail += `\n优惠券: ${rinfo.name}: ${rinfo.amount}元 (${rinfo.condition})`
-                else detail += `\n未知奖励!`
+            if (result.code == 0) {
+                subTitle = '签到结果: 成功'
+                detail = `本周连签: ${result.data[0].circleSignTimes}天`
+                for (r of result.data[0].signInAwardRecords) {
+                    const rinfo = JSON.parse(r.info)
+                    if (rinfo.type == `cash`) detail += `, 奖励金: ${rinfo.amount / 100}元`
+                    else if (rinfo.type == `coupon`) detail += `\n优惠券: ${rinfo.name}: ${rinfo.amount}元 (${rinfo.condition})`
+                    else detail += `\n未知奖励!`
+                }
+            } else if (result.code == 2002) {
+                subTitle = `签到结果: 成功 (重复签到)`
+                detail = `说明: ${result.msg}`
+            } else {
+                subTitle = `签到结果: 失败`
+                detail = `编码: ${result.code}, 说明: ${result.msg}`
             }
-        } else if (result.code == 2002) {
-            subTitle = `签到结果: 成功 (重复签到)`
-            detail = `说明: ${result.msg}`
+            qxApi.msg(cookieName, subTitle, detail)
         } else {
-            subTitle = `签到结果: 失败`
-            detail = `编码: ${result.code}, 说明: ${result.msg}`
+            qxApi.msg(cookieName, "签到结果: 失败", "响应数据为空或解析错误")
         }
-        qxApi.msg(cookieName, subTitle, detail)
         qxApi.done()
     })
 }
