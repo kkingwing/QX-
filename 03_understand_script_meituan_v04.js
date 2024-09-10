1d557e8352924794520a0067ed703438d8336f07
Qing Yi <495017921@qq.com>
2024-09-10 20:24:58 +0800

    fix

--- 03_understand_script_meituan_v04.js
+++ 03_understand_script_meituan_v04.js
@@ -12,7 +12,6 @@ const signurlKey = 'qingyi_signurl_meituan'
 const signheaderKey = 'qingyi_signheader_meituan'
 const signbodyKey = 'qingyi_signbody_meituan'
 const qxApi = init()
-
 const tokenurlVal = qxApi.getdata(tokenurlKey)
 const tokenheaderVal = qxApi.getdata(tokenheaderKey)
 const signurlVal = qxApi.getdata(signurlKey)
@@ -34,25 +33,19 @@ function sign() {
         if (result.code == 0) {
             subTitle = '签到结果: 成功'
             detail = `本周连签: ${result.data[0].circleSignTimes}天`
-
-            result.data[0].signInAwardRecords.forEach(record => {
-                const rinfo = JSON.parse(record.info)
-                if (rinfo.type == 'cash') {
-                    detail += `, 奖励金: ${rinfo.amount / 100}元`
-                } else if (rinfo.type == 'coupon') {
-                    detail += `\n优惠券: ${rinfo.name}: ${rinfo.amount}元 (${rinfo.condition})`
-                } else {
-                    detail += `\n未知奖励!`
-                }
-            })
+            for (r of result.data[0].signInAwardRecords) {
+                const rinfo = JSON.parse(r.info)
+                if (rinfo.type == `cash`) detail += `, 奖励金: ${rinfo.amount / 100}元`
+                else if (rinfo.type == `coupon`) detail += `\n优惠券: ${rinfo.name}: ${rinfo.amount}元 (${rinfo.condition})`
+                else detail += `\n未知奖励!`
+            }
         } else if (result.code == 2002) {
-            subTitle = '签到结果: 成功 (重复签到)'
+            subTitle = `签到结果: 成功 (重复签到)`
             detail = `说明: ${result.msg}`
         } else {
-            subTitle = '签到结果: 失败'
+            subTitle = `签到结果: 失败`
             detail = `编码: ${result.code}, 说明: ${result.msg}`
         }
-
         qxApi.msg(cookieName, subTitle, detail)
         qxApi.done()
     })
@@ -60,18 +53,33 @@ function sign() {
 
 
 function init() {
-    getdata = (key) => $prefs.valueForKey(key)
-    setdata = (key, val) => $prefs.setValueForKey(key, val)
-    msg = (title, subtitle, body) => $notify(title, subtitle, body)
+    isQuanX = () => {
+        return undefined === this.$task ? false : true
+    }
+    getdata = (key) => {
+        if (isQuanX()) return $prefs.valueForKey(key)
+    }
+    setdata = (key, val) => {
+        if (isQuanX()) return $prefs.setValueForKey(key, val)
+    }
+    msg = (title, subtitle, body) => {
+        if (isQuanX()) $notify(title, subtitle, body)
+    }
     log = (message) => console.log(message)
     get = (url, cb) => {
-        url.method = 'GET'
-        $task.fetch(url).then(res => cb(null, {}, res.body))
+        if (isQuanX()) {
+            url.method = 'GET'
+            $task.fetch(url).then((resp) => cb(null, resp, resp.body))
+        }
     }
     post = (url, cb) => {
-        url.method = 'POST'
-        $task.fetch(url).then(res => cb(null, {}, res.body))
+        if (isQuanX()) {
+            url.method = 'POST'
+            $task.fetch(url).then((resp) => cb(null, resp, resp.body))
+        }
     }
-    done = (value = {}) => $done(value)
-    return {getdata, setdata, msg, log, get, post, done}
+    done = (value = {}) => {
+        $done(value)
+    }
+    return {isSurge, isQuanX, msg, log, getdata, setdata, get, post, done}
 }
