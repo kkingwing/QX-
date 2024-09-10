e2992b327f775070375c8beaa6321af4e217ba05
Qing Yi <495017921@qq.com>
2024-09-10 15:33:05 +0800

    fix

--- 02_test_meituan_v03.js
+++ 02_test_meituan_v03.js
@@ -71,11 +71,10 @@ if ($request && $request.method != 'OPTIONS' && resUrl.match(urlPattern)) {
     if (signurlVal) qxApi.setdata(signurlVal, signurlKey)
     if (signheaderVal) qxApi.setdata(signheaderVal, signheaderKey)
     if (signbodyVal) qxApi.setdata(signbodyVal, signbodyKey)
-    //调试
+    //调试，输出变量需要用 '${xxx}'  来表达
     qxApi.msg(cookieName, `获取Cookie: 成功`, `请求URL: ${signurlVal}`)
-    qxApi.msg(cookieName, '测试内容：', (`\n\n===========\n`, cookieName,
-        `\n获取的url:\n`, signurlVal, `\n请求头:\n`, signheaderVal, `\n请求体:\n`, signbodyVal,
-        `\n===========\n\n`)
+    qxApi.msg(cookieName, '测试内容：', (`\n\n===========\n`,
+        `\n获取的url:\n${signurlVal}`, `\n请求头:\n${signheaderVal}`, `\n请求体:\n${signbodyVal} n===========\n\n`)
     )
 }
 
